b4c6deb957a481852df6d24000ffd735194d8e9b
vs.almeida <vs.almeida@icloud.com>
2018-05-02 13:04:24 -0400

    alterando travis.yml

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek!";
+        return "HelloWorld , testando springboot com CodeAnywhere, GitHub , Travis CI e Heroku";
     }
 
 }
